fad6f4174a71b16f442efba64989dc7890283647
RobertHan <roberthan1125@gmail.com>
2025-10-15 20:04:13 +0900

    Simplify deployment config - keep only essential files

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set default port if PORT is not set
-export PORT=${PORT:-8080}
-
-# Start the application
-exec gunicorn ix.web.app:server --bind 0.0.0.0:$PORT --workers 4 --timeout 120
